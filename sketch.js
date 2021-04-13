05b245ffec0d6e3eba9f15f0562264456df08f55
Thao-Nguyen-Design <72170881+Thao-Nguyen-Design@users.noreply.github.com>
2021-04-12 22:44:44 -0700

    inprogress

--- sketch.js
+++ sketch.js
@@ -1,15 +1,34 @@
-// let array = [];
-var modeChoice = '1';
-let noiseOffset = 0.0;
-let strokeSize = 5;
+let circleArray = [];
+// var modeChoice = '1';
+
+// let symmetry = 1;
+// let strokeSlider;
+let waiting = true;
+let colorchange = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(90);
+
+  colorMode(HSB, 360, 100, 100, 100);
+
   textAlign(CENTER);
   ellipseMode(CENTER);
   textSize(32);
   angleMode(DEGREES);
-  bgColor = color(0, 0 , 0, 5);
+  bgColor = color(0);
+  background(bgColor);
+
+  // noFill();
+  // stroke(255);
+  // strokeWeight(5);
+  // rect(0,0, 350, 600);
+
+  strokeSlider = select("#strokeSlider");
+  strokeSlider.class("strokeSlider");
+
+  symmetrySlider = select("#symmetrySlider");
+  symmetrySlider.class("symmetrySlider");
 
   saveButton = select("#saveButton")
   saveButton.mousePressed(saveMe);
@@ -22,84 +41,116 @@ function setup() {
 
 
 function draw() {
-  background(bgColor);
 
   x = mouseX;
   y = mouseY;
   px = pmouseX;
   py = pmouseY;
 
-  if (x > width * 0.3 || y > height * 0.3) {
-    if (keyIsPressed) {
-      modeChoice = key;
-    }
-    if (mouseIsPressed) {
-      newModeChoice(modeChoice);
-    }
+if (waiting == true) {
+  waitingScreen();
+}
+
+  if ((x > 350 || y > 600) && mouseIsPressed) {
+    waiting = false;
+    startdraw();
   }
 }
 
 
-  function newModeChoice(modeChoice) {
+function startdraw() {
 
-    if (modeChoice == '1') {
+  translate(width / 2, height / 2);
 
-      // stroke(random(255), random(255), random(255));
-      strokeWeight(strokeSize);
+  let strokevalue = noise(colorchange) * 360;
 
-      noiseOffset += 0.05;
-      strokeSize = noise(noiseOffset) * 100;
+  //make fading
+  let alphavalue = map(x, 0, width, 0, 100);
+  let saturationvalue = map(x, 0, width, 0, 100);
 
-      stroke(map(mouseX, 0, 600, 0, 255, true));
-      line(width - x, height - y, width - px, height - py);
-      line(x, y, px, py);
+  colorchange += 0.05;
 
-    } else if (modeChoice == '2') {
-      noFill();
-      strokeWeight(3);
-      stroke(255);
-      translate(width / 2, height / 2);
-      for (var i = 0; i < 16; i++) {
-        rotate(360 / 10);
-        ellipse(x - width / 2, y - height / 2, px, py, 50);
-      }
+  stroke(strokevalue, saturationvalue, 100, alphavalue);
 
-    } else if (modeChoice == '3') {
+  let symmetry = symmetrySlider.value();
+  let rotateAngle = 360 / symmetry;
 
-      strokeWeight(3);
-      stroke(random(255), random(255), random(255));
+  for (let i = 0; i < symmetry; i++) {
+    rotate(rotateAngle);
+    let sw = strokeSlider.value();
+    strokeWeight(sw);
+    line(x - width / 2, y - height / 2, px - width / 2, py - height / 2);
 
-      line(x, y, px, py);
-      line(width - x, y, width - px, py);
-      line(x, height - y, px, height - py);
-      line(width - x, height - y, width - px, height - py);
+    push();
+    scale(-1, 1);
+    line(x - width / 2, y - height / 2, px - width / 2, py - height / 2);
+    pop();
 
-    } else if (modeChoice == '4') {
-
-      // noFill();
-      // strokeWeight(3);
-      // stroke(0);
-      translate(width / 2, height / 2);
-      for (let i = 0; i < 6; i++) {
-        rotate(i * 60);
-        line(x - width / 2, y - height / 2, px - width / 2, py - height / 2);
-      }
-    }
+  }
+}
 
 
-  };
-  //    }
-  //
-  // }
-  function saveMe() {
-    saveCanvas('design', 'png');
+function waitingScreen(){
+waiting = true;
+  background(bgColor);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].display();
+    circleArray[i].move();
+
   }
 
-  function clearMe() {
-    clear();
-    modeChoice = '1';
-  }
+}
 
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
+
+// } else if (modeChoice == '2') {
+//   noFill();
+//   strokeWeight(3);
+//   stroke(255);
+//   translate(width / 2, height / 2);
+//   for (var i = 0; i < 16; i++) {
+//     rotate(360 / 10);
+//     ellipse(x - width / 2, y - height / 2, px, py, 50);
+//   }
+//
+// } else if (modeChoice == '3') {
+//
+//   strokeWeight(3);
+//   stroke(random(255), random(255), random(255));
+//
+//   line(x, y, px, py);
+//   line(width - x, y, width - px, py);
+//   line(x, height - y, px, height - py);
+//   line(width - x, height - y, width - px, height - py);
+//
+// } else if (modeChoice == '4') {
+//
+//   // noFill();
+//   // strokeWeight(3);
+//   // stroke(0);
+//   translate(width / 2, height / 2);
+//   for (let i = 0; i < 6; i++) {
+//     rotate(i * 60);
+//     line(x - width / 2, y - height / 2, px - width / 2, py - height / 2);
+//   }
+// }
+//
+// }
+// };
+//    }
+//
+// }
+function saveMe() {
+  saveCanvas('mydesign', 'png');
+}
+
+function clearMe() {
+  background(bgColor);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(bgColor);
+
+}
